f78e003ddd67ea5e2147beb642798b24b762fd81
Tim Cole <tscole@inspiredwd.com>
2019-03-26 14:08:57 +0000

    Add amplify vue and first version of api schema

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Horseman
 Open source, headless, serverless CMS
+
+1. Install amplify cli:
+2. amplify configure
+3. amplify init
--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { components } from 'aws-amplify-vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+    ...components
   }
 }
 </script>
